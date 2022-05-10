c39e54ed5b9b29ab1fb365974f77cef55a94d08d
chry8822 <goggg8822@naver.com>
2022-05-10 14:16:28 +0900

    add UserList component, state lifting, add key

--- practice react project/src/App.js
+++ practice react project/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './componenets/Users/AddUser';
-
+import UserList from './componenets/Users/UserList';
 
 function App() {
+
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, { name: userName, age: userAge, id: Math.random().toString() }]
+    });
+  };
+  // 이전 상태에 의존해서 상태를 업데이트 할때는 useState에 setter 함수의 대체 폼을 사용하는데 setter 함수에 함수를 전달하는 폼을 사용한다. 그리고 리액트가 여기서 상태를 업데이트할 때 그 함수는 자동으로 이전 상태 중 가장 최신의 스냅샷을 가지고 온다.
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
--- practice react project/src/componenets/Users/AddUser.js
+++ practice react project/src/componenets/Users/AddUser.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
+  const [enterUserName, setEnterUserName] = useState('');
+  const [enterAge, setEnterAge] = useState('');
+  // useState는 항상 두개의 요소가 있는 배열을 반환
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    // trim.()은 문자열 앞뒤의 공백을 제거한다.
+    if (enterUserName.trim().length === 0 || enterAge.trim().length === 0) {
+      return;
+    }
+    if (+enterAge < 1) {
+      return;
+    }
+    props.onAddUser(enterUserName, enterAge)
+    setEnterUserName('')
+    setEnterAge('')
+  }
 
+  const usernameChangeHandler = event => {
+    setEnterUserName(event.target.value)
+  }
 
+  const ageChangeHandler = event => {
+    setEnterAge(event.target.value)
   }
 
   return (
@@ -16,9 +36,10 @@ const AddUser = (props) => {
       <form onSubmit={addUserHandler}>
         {/* 버튼(submit)으로 전송하게 되면 폼이 제출되었을 때 onSubmit 속성에 지정한 함수를 실행한다 */}
         <label htmlFor='username'>Username</label>
-        <input id='username' type="text" />
+        <input id='username' type="text" value={enterUserName} onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type="number" />
+        <input id='age' type="number" value={enterAge} onChange={ageChangeHandler} />
+        {/* input 태그의 value 속성은 키입력으로 변경되는게 아니라 폼을 제출하는 경우에도 현재 상태의 스냅샷을 반영하는 입력값을 가질수 있다.*/}
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
