9518c875832ada594af3af5fa10153bbcf1a9a0c
chry8822 <goggg8822@naver.com>
2022-05-17 21:28:45 +0900

    add modal component

--- practice react project/src/componenets/Users/AddUser.js
+++ practice react project/src/componenets/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 
 import classes from './AddUser.module.css'
 
@@ -8,14 +9,23 @@ const AddUser = (props) => {
   const [enterUserName, setEnterUserName] = useState('');
   const [enterAge, setEnterAge] = useState('');
   // useState는 항상 두개의 요소가 있는 배열을 반환
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     // trim.()은 문자열 앞뒤의 공백을 제거한다.
     if (enterUserName.trim().length === 0 || enterAge.trim().length === 0) {
+      setError({
+        title: '입력값이 유효 하지 않습니다.',
+        message: '유효한 이름과 나이를 입력해 주세요.(빈칸X)',
+      })
       return;
     }
     if (+enterAge < 1) {
+      setError({
+        title: '나이가 유효하지 않습니다.',
+        message: '유효한 나이를 입력해 주세요( > 0)',
+      })
       return;
     }
     props.onAddUser(enterUserName, enterAge)
@@ -31,18 +41,25 @@ const AddUser = (props) => {
     setEnterAge(event.target.value)
   }
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        {/* 버튼(submit)으로 전송하게 되면 폼이 제출되었을 때 onSubmit 속성에 지정한 함수를 실행한다 */}
-        <label htmlFor='username'>Username</label>
-        <input id='username' type="text" value={enterUserName} onChange={usernameChangeHandler} />
-        <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type="number" value={enterAge} onChange={ageChangeHandler} />
-        {/* input 태그의 value 속성은 키입력으로 변경되는게 아니라 폼을 제출하는 경우에도 현재 상태의 스냅샷을 반영하는 입력값을 가질수 있다.*/}
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+    <>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+      < Card className={classes.input} >
+        <form onSubmit={addUserHandler}>
+          {/* 버튼(submit)으로 전송하게 되면 폼이 제출되었을 때 onSubmit 속성에 지정한 함수를 실행한다 */}
+          <label htmlFor='username'>Username</label>
+          <input id='username' type="text" value={enterUserName} onChange={usernameChangeHandler} />
+          <label htmlFor='age'>Age (Years)</label>
+          <input id='age' type="number" value={enterAge} onChange={ageChangeHandler} />
+          {/* input 태그의 value 속성은 키입력으로 변경되는게 아니라 폼을 제출하는 경우에도 현재 상태의 스냅샷을 반영하는 입력값을 가질수 있다.*/}
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card >
+    </>
   )
 }
 
--- practice react project/src/componenets/Users/UserList.js
+++ practice react project/src/componenets/Users/UserList.js
@@ -5,13 +5,13 @@ import classes from './UserList.module.css'
 
 const UserList = props => {
 
-
   return (
     <Card className={`${classes.users} ${props.className}`}>
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
             {user.name} ({user.age} years old)
+            <button>Delelte</button>
           </li>
         ))}
       </ul>
